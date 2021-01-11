0a6b1db3e0e20f5f74f9e581ee9b5eafeaf920c6
wfx <wolfgang.morawetz@gmail.com>
2021-01-11 14:41:32 +0100

    initital l10n

--- boss.py
+++ boss.py
@@ -43,6 +43,7 @@
 #   https://systemd.io/DISCOVERABLE_PARTITIONS/
 #   https://jlk.fjfi.cvut.cz/arch/manpages/man/systemd-gpt-auto-generator.8
 #   https://en.wikipedia.org/wiki/GUID_Partition_Table#Partition_type_GUIDs
+#   https://www.freedesktop.org/wiki/Specifications/DiscoverablePartitionsSpec/
 #
 
 __author__ = "Wolfgang Morawetz"
@@ -53,6 +54,7 @@ __github__ = "https://github.com/wfx/boss"
 __source__ = "Source code and bug reports: {0}".format(__github__)
 
 import yaml
+import requests
 
 
 class Blockdevices:
@@ -62,6 +64,7 @@ class Blockdevices:
         self.mount_list = []
         gpt_typecodes = {
             'esp': 'C12A7328-F81F-11D2-BA4B-00A0C93EC93B',
+            'boot': 'BC13C2FF-59E6-4262-A352-B275FD6F7172',
             'swap': '0657FD6D-A4AB-43C4-84E5-0933C84B4F4F',
             'root_x86-64': '4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709',
             'home': '933AC7E1-2EB4-4F13-B844-0E14E2AEF915',
@@ -70,7 +73,8 @@ class Blockdevices:
         for device, dev in self.template.get('Mapping').items():
             # Using section 'Mapping' to generate the part and mount list.
             for partition, part in self.template[device].items():
-                self.mount_list.append({'device':device, 'partition':partition})
+                self.mount_list.append(
+                    {'device': device, 'partition': partition})
                 self.part_list.append(self.mount_list[-1])
                 if part.get('type')[0:4] == "root":
                     try:
@@ -79,14 +83,19 @@ class Blockdevices:
                     except ValueError:
                         print(f"Moving item to 0 failed. List remains unchanged.")
                 # Modify and adding template settings.
-                self.template[device][partition].update({'device_name':dev['name']})
-                self.template[device][partition].update({'partition_name':dev['name'] + partition[-1]})
-                self.template[device][partition].update({'label':part.get('label', part.get('type')[0:4]).upper()})
+                self.template[device][partition].update(
+                    {'device_name': dev['name']})
+                self.template[device][partition].update(
+                    {'partition_name': dev['name'] + partition[-1]})
+                self.template[device][partition].update(
+                    {'label': part.get('label', part.get('type')[0:4]).upper()})
                 # Maybe overwrite the type setting.
-                self.template[device][partition].update({'gpt_type':gpt_typecodes.get(part['type'], 'ERROR: Unknown partition type')})
+                self.template[device][partition].update(
+                    {'gpt_type': gpt_typecodes.get(part['type'], 'ERROR: Unknown partition type')})
                 # Crypt...
                 if part.get('crypt') == "luks":
-                    self.template[device][partition].update({'crypt_mount_point': part['label'].lower()})
+                    self.template[device][partition].update(
+                        {'crypt_mount_point': part['label'].lower()})
 
     def make_partition(self):
         print("# Make Partitions:...")
@@ -103,14 +112,15 @@ class Blockdevices:
                 print(f"cryptsetup luksFormat {_name}")
                 print(f"cryptsetup open {_name} {_['crypt_mount_point']}")
                 _name = "/dev/mapper/" + _['crypt_mount_point']
-            if _['format'] == "vfat32":
+            _format = _.get('format')
+            if _format == "vfat32":
                 print(f"mkfs.vfat -F32 -n {_['label']} {_name}")
-            elif _['format'] == "swap":
+            elif _format == "swap":
                 print(f"mkfs.swap -L {_['label']} {_name}")
-            elif _['format'] == "btrfs":
+            elif _format == "btrfs":
                 print(f"mkfs.btrfs -f -L {_['label']} {_name}")
                 self.make_btrfs_subvolumes(_name, _.get('Subvolumes'))
-            elif _['format'] == "ext4":
+            elif _format == "ext4":
                 print(f"mkfs.ext4 -f -L {_['label']} {_name}")
             else:
                 print("ERROR: Unknown filesystem!")
@@ -129,13 +139,15 @@ class Blockdevices:
             _name = "/dev/" + _['partition_name']
             if _.get('crypt') == "luks":
                 _name = "/dev/mapper/" + _['crypt_mount_point']
-            if _['format'] == "vfat32":
-                print(f"mount {_name} {_.get('mount_point', 'ERROR: Missing mount point')}")
-            elif _['format'] == "swap":
+            _format = _.get('format')
+            if _format == "vfat32":
+                print(
+                    f"mount {_name} {_.get('mount_point', 'ERROR: Missing mount point')}")
+            elif _format == "swap":
                 print(f"swapon {_name}")
-            elif _['format'] == "btrfs":
+            elif _format == "btrfs":
                 self.mount_btrfs_subvolumes(_name, _.get('Subvolumes'))
-            elif _['format'] == "ext4":
+            elif _format == "ext4":
                 print(f"mount {_name} {_['mount_point']} ")
             else:
                 print("ERROR: Unknown filesystem!")
@@ -152,15 +164,46 @@ class Blockdevices:
                 print(f"mount -o {sub.get('mount_option', 'noatime,nodiratime')},subvol={sub.get('subvolume')} {part_name} /mnt{_mp}")
 
 
+class Localization:
+    def __init__(self, localization):
+        self.l10n = localization
+        url = "https://freegeoip.app/json/"
+        headers = {'accept': "application/json",
+                   'content-type': "application/json"}
+        # self.geoip = requests.request("GET", url, headers=headers)
+
+    def keyboard(self):
+        kb = self.l10n.get('keyboard')
+        if kb:
+            print(f"loadkeys {kb}")
+
+    def time_zone(self):
+        tz = self.l10n.get('time_zone', self.geoip.json()['time_zone'])
+        print(
+            f"arch-chroot /mnt/ ln -sf /usr/share/zoneinfo/{tz} /etc/localtime")
+
+
 class Application:
     def __init__(self, template):
         self.template = self.load_template(template)
+        self.system = self.template.get('System')
+        self.l10n = Localization(self.template.get('Localization'))
         self.blkd = Blockdevices(self.template.get('Blockdevices'))
 
     def run(self):
+        self.l10n.keyboard()
+        print(f"timedatectl set-ntp {self.system['system_clock']}")
         self.blkd.make_partition()
         self.blkd.make_filesystem()
         self.blkd.mount_partition()
+        print("pacman -Sy archlinux-keyring")
+        print("pacman-key --populate archlinux")
+        print("pacman-key --refresh-keys")
+        print(f"pacstrap /mnt {self.system['pacstrap']}")
+        print("genfstab -U /mnt >> /mnt/etc/fstab")
+        print(f"echo {self.system['hostname']} > /mnt/etc/hostname")
+        # Locale...
+        self.l10n.time_zone()
 
     def load_template(self, template):
         with open(template, 'r') as file:
